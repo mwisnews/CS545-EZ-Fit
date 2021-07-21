f715de7dacd098ce87f90b3ac4e816b0fd18cdef
mwisnews <mwisnews@stevens.edu>
2021-07-20 22:51:21 -0400

    Login and Home Page
    Created Login and Home Pages. Created package.json file and added depedencies needed to run the program.

---
+++ routes/login.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+const trim = (str) => (str || "").trim();
+
+router.get("/", async (req, res) => res.redirect("/login"));
+
+router.get("/login", async (req, res) => {
+  try {
+    res.render("pages/login", { title: "Login" });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        res.render("pages/home", { title: "Home Page" });
+      } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+      }
+  });
+
+router.get("/newUser", async (req, res) => {
+  try {
+    res.render("pages/newUser", { title: "Sign Up" });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.use(async (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+});
+
+//DEV FOR LOGOUT
+router.get("/logout", async (req, res) => {
+  req.session.destroy();
+  res.render("pages/logout", { title: "Log Out" });
+});
+
+module.exports = router;
